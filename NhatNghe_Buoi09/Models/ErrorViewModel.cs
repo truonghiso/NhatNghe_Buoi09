06e70a2d768283845c26a213e8b57e0d2df3ff08
NhatNghe_Buoi09_Validate <truong.nguyennhat1997@gmail.com>
2020-06-07 09:59:14 +0700

    Học Validate, update 1

---
+++ NhatNghe_Buoi09/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NhatNghe_Buoi09.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
